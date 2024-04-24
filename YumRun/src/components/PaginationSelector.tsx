1db43b47e104275268602bead64ccf7809f00903
mohmedbm <mohmedbamhrz@gmail.com>
2024-04-24 21:26:46 +0300

    Finish working on cuisine filter
    - Finished the work in the side cuisine areea
    - Fixed a bug where the pages count number was not shown
      correctly
    - Fixed a bug where the reset button was not wokring correctly

---
+++ YumRun/src/components/PaginationSelector.tsx
@@ -0,0 +1,69 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./ui/pagination";
+
+type Props = {
+  page: number;
+  pages: number;
+  onPageChange: (page: number) => void;
+  filteredResults: number;
+};
+
+const PaginationSelector = ({
+  page,
+  pages,
+  onPageChange,
+  filteredResults,
+}: Props) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= pages; i++) {
+    pageNumbers.push(i);
+  }
+
+  console.log(page);
+
+  // Hide pagination controls if filtered results are 0
+  if (filteredResults === 0) {
+    return null;
+  }
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        {page !== 1 && (
+          <PaginationItem>
+            <PaginationPrevious
+              href="#"
+              onClick={() => onPageChange(page - 1)}
+            />
+          </PaginationItem>
+        )}
+
+        {pageNumbers.map((number) => (
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              onClick={() => onPageChange(number)}
+              isActive={page === number}
+            >
+              {number}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
+        {page !== pageNumbers.length && (
+          <PaginationItem>
+            <PaginationNext href="#" onClick={() => onPageChange(page + 1)} />
+          </PaginationItem>
+        )}
+      </PaginationContent>
+    </Pagination>
+  );
+};
+
+export default PaginationSelector;
--- YumRun/src/pages/SearchPage.tsx
+++ YumRun/src/pages/SearchPage.tsx
@@ -1,28 +1,36 @@
-import SearchBar, { SearchForm } from '@/components/SearchBar';
-import SearchResultCard from '@/components/SearchResultCard';
-import SearchResultInfo from '@/components/SearchResultInfo';
-import { db } from '@/firebase';
-import { collection, onSnapshot } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import CuisineFilter from "@/components/CuisineFilter";
+import PaginationSelector from "@/components/PaginationSelector";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import SearchResultCard from "@/components/SearchResultCard";
+import SearchResultInfo from "@/components/SearchResultInfo";
+import { db } from "@/firebase";
+import { collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { set } from "react-hook-form";
+import { useParams } from "react-router-dom";
 
 export type SearchState = {
   searchQuery: string;
-}
+  page: number;
+};
 
 export const SearchPage = () => {
   const { city } = useParams();
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [originalResults, setOriginalResults] = useState([]);
-  const [SearchState, setSearchState] = useState<SearchState>({
-    searchQuery: '',
+  const [filteredResults, setFilteredResults] = useState([]); // New state for filtered results
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+    page: 1,
   });
-
+  const [selectedCuisines, setSelectedCuisines] = useState<string[]>([]); // New state to store selected cuisines
+  const [totalPages, setTotalPages] = useState(1); // State to store total pages
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
   useEffect(() => {
     const handleSearch = async () => {
-      const storesRef = collection(db, 'stores');
+      const storesRef = collection(db, "stores");
       // Subscribe to real-time updates
       const unsubscribe = onSnapshot(storesRef, async (querySnapshot) => {
         const storesData = [];
@@ -31,9 +39,11 @@ export const SearchPage = () => {
           storesData.push({ ...storeData, id: doc.id });
         }
         // Filter stores by city
-        const filteredData = storesData.filter((store) => store.city.toLowerCase() === city.toLowerCase());
-        setResults(filteredData);
+        const filteredData = storesData.filter(
+          (store) => store.city.toLowerCase() === city.toLowerCase()
+        );
         setOriginalResults(filteredData);
+        setFilteredResults(filteredData); // Initialize filtered results with original data
         setIsLoading(false); // Set loading to false once data is fetched
       });
       // Cleanup function to unsubscribe when component unmounts
@@ -44,51 +54,115 @@ export const SearchPage = () => {
     handleSearch();
   }, [city]);
 
+  useEffect(() => {
+    const startIndex = (searchState.page - 1) * 5;
+    const endIndex = startIndex + 5;
+    setResults(filteredResults.slice(startIndex, endIndex));
+  }, [searchState.page, filteredResults]);
+
+  useEffect(() => {
+    // Calculate total pages dynamically
+    const totalPages = Math.ceil(filteredResults.length / 5);
+    setTotalPages(totalPages);
+  }, [filteredResults]);
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      page,
+    }));
+  };
+
   const setSearchQuery = (searchFormData: SearchForm) => {
-    // filter result by the search query using cusines array and name
-    const filteredResults = results.filter((store) => {
+    // Filter results by the search query using cuisines array and name
+    const filteredResults = originalResults.filter((store) => {
       const cuisines = store.cuisines.map((cuisine) => cuisine.toLowerCase());
       return (
-        store.storeName.toLowerCase().includes(searchFormData.searchQuery.toLowerCase()) ||
-        cuisines.some((cuisine) => cuisine.includes(searchFormData.searchQuery.toLowerCase()))
+        store.storeName
+          .toLowerCase()
+          .includes(searchFormData.searchQuery.toLowerCase()) ||
+        cuisines.some((cuisine) =>
+          cuisine.includes(searchFormData.searchQuery.toLowerCase())
+        )
       );
     });
 
-    setResults(filteredResults);
-  }
+    // Update results with filtered results
+    setResults(filteredResults.slice(0, 5));
+    setFilteredResults(filteredResults); // Update filtered results state
 
+    // Reset page to 1 after filtering
+    setSearchState((prevState) => ({
+      ...prevState,
+      page: 1,
+    }));
+  };
+
+  // Reset search functionality
   const resetSearch = () => {
-    setResults(originalResults);
-  }
+    // Reset search query and results to original state
+    setResults(originalResults.slice(0, 5)); // Reset to first page
+    setFilteredResults(originalResults); // Reset filtered results to original data
+    setSearchState({
+      searchQuery: "",
+      page: 1,
+    });
+  };
 
-  if ((!results || !city)) {
-    return (<span>No results found</span>)
+  const handleSelectedCuisinesChange = (cuisines: string[]) => {
+    setSelectedCuisines(cuisines);
+    //filter by selected cuisines
+    const filteredResults = originalResults.filter((store) => {
+      return cuisines.every((cuisine) => store.cuisines.includes(cuisine));
+    });
+    setFilteredResults(filteredResults);
+    // Reset page to 1 after filtering
+    setSearchState((prevState) => ({
+      ...prevState,
+      page: 1,
+    }));
+  };
+
+  if (!results || !city) {
+    return <span>No results found</span>;
   }
 
   if (isLoading) {
-    return (<span>Loading...</span>)
+    return <span>Loading...</span>;
   }
-
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-      <div id='cuisines-list'>
-        Insert Cuisines List Here (TODO)
+      <div id="cuisines-list">
+        <CuisineFilter
+          selectedCuisines={selectedCuisines}
+          onChange={handleSelectedCuisinesChange}
+          isExpanded={isExpanded}
+          onExpandedClick={() =>
+            setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+          }
+        />
       </div>
 
       <div className="flex flex-col gap-5">
         <SearchBar
-          searchQuery={SearchState.searchQuery}
+          searchQuery={searchState.searchQuery}
           onSubmit={setSearchQuery}
-          placeholder='Search by store name, cuisine...'
+          placeholder="Search by store name, cuisine..."
           onReset={resetSearch}
         />
-        <SearchResultInfo total={results.length} city={city} />
+        <SearchResultInfo total={filteredResults.length} city={city} />
         {results.map((store) => (
-          <SearchResultCard store={store} />
+          <SearchResultCard key={store.id} store={store} />
         ))}
-      </div>
 
+        <PaginationSelector
+          filteredResults={filteredResults.length}
+          page={searchState.page}
+          pages={totalPages} // Use totalPages state
+          onPageChange={setPage}
+        />
+      </div>
     </div>
   );
 };
