152f8bb3d82dce552950cb0fd27a6aca39cd2bb4
mohmedbm <mohmedbamhrz@gmail.com>
2024-04-26 21:41:41 +0300

    - Start MenuItem page

---
+++ YumRun/src/components/StoreInfo.tsx
@@ -0,0 +1,38 @@
+import { Store } from "@/types";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Dot } from "lucide-react";
+
+type Props = {
+  store: Store;
+};
+
+export const StoreInfo = ({ store }: Props) => {
+  console.log(store);
+  return (
+    <Card className="border-sla">
+      <CardHeader>
+        <CardTitle className="text-3xl font-bold tracking-tight">
+          {store.storeName}
+        </CardTitle>
+        <CardDescription>
+          {store.city}, {store.country}
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="flex">
+        {store.cuisines.map((cuisine, index) => (
+          <span className="flex">
+            <span>{cuisine}</span>
+            {index < store.cuisines.length - 1 && <Dot />}
+          </span>
+        ))}
+      </CardContent>
+    </Card>
+  );
+};
--- YumRun/src/pages/DetailPage.tsx
+++ YumRun/src/pages/DetailPage.tsx
@@ -1,3 +1,6 @@
+import { MenuItem } from "@/components/MenuItem";
+import { StoreInfo } from "@/components/StoreInfo";
+import { Store } from "@/types";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import { AspectRatioIcon } from "@radix-ui/react-icons";
 import React, { useEffect, useState } from "react";
@@ -5,8 +8,8 @@ import { useParams } from "react-router-dom";
 
 export const DetailPage = ({}) => {
   const { id } = useParams();
-  const [store, setStore] = useState(null);
-  const [isGetLoading, setisGetLoading] = useState(false);
+  const [store, setStore] = useState<Store>(null);
+  const [isGetLoading, setisGetLoading] = useState(true);
 
   const fetchStore = async () => {
     setisGetLoading(true);
@@ -32,12 +35,29 @@ export const DetailPage = ({}) => {
   useEffect(() => {
     fetchStore();
   }, []);
-  console.log(store);
+
   if (isGetLoading) return <div>Loading...</div>;
 
   return (
-    <div className="flex flex-col gap10">
-      <AspectRatio ratio={16 / 5}></AspectRatio>
+    <div className="flex flex-col gap-10">
+      <h1>{id}</h1>
+      <AspectRatio ratio={16 / 5}>
+        <img
+          src={store?.imageFile}
+          alt=""
+          className="rounded-md object-cover h-full w-full"
+        />
+      </AspectRatio>
+
+      <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
+        <div className="flex flex-col gap-4">
+          <StoreInfo store={store} />
+          <span className="text-2xl font-bold tracking-tightt">Menu</span>
+          {store.menuItems.map((menuItem) => (
+            <MenuItem menuItem={menuItem} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
